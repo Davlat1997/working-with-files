501c41a41baa1203a14d741dbe7a1a3574040c9e
Davlat1997 <aslamovavazira818@gmail.com>
2024-12-19 20:55:13 +0500

    done

---
+++ 1.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main() {
+    FILE *f;
+    int num, sum = 0, count = 0;
+    float urtacha;
+
+  
+    f = fopen("output.txt", "r");
+    if (f == NULL) {
+        printf("Faylni ochib bo'lmadi.\n");
+        return 1;
+    }
+
+
+    while (fscanf(f, "%d", &num) == 1) {
+        sum += num;
+        count++;
+    }
+
+   
+    if (count > 0) {
+        urtacha = (float)sum / count;
+        printf("Sonlarning o'rta arifmetigi: %.2f\n", urtacha);
+    } else {
+        printf("Faylda sonlar topilmadi.\n");
+    }
+
+  
+    fclose(f);
+    return 0;
+}
+    
