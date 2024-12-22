91369f67eef76930a6ce1e329d9ef5432df3291b
Davlat1997 <aslamovavazira818@gmail.com>
2024-12-22 09:27:52 +0500

    done

---
+++ 5.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+int main() {
+    FILE *inputFile, *outputFile;
+    int num;
+
+
+    inputFile = fopen("input.txt", "r");
+    if (inputFile == NULL) {
+        
+        return 1;
+    }
+
+  
+    outputFile = fopen("output.txt", "w");
+    if (outputFile == NULL) {
+        fclose(inputFile);
+        return 1;
+    }
+
+   
+    fprintf(outputFile, "Juft sonlar: ");
+    while (fscanf(inputFile, "%d", &num) != EOF) {
+        if (num % 2 == 0) {
+            fprintf(outputFile, "%d ", num);
+        }
+    }
+
+   
+    rewind(inputFile);
+    fprintf(outputFile, "\nToq sonlar: ");
+    while (fscanf(inputFile, "%d", &num) != EOF) {
+        if (num % 2 != 0) {
+            fprintf(outputFile, "%d ", num);
+        }
+    }
+
+  
+    fclose(inputFile);
+    fclose(outputFile);
+
+    printf("Juft va toq sonlar output.txt fayliga yozildi.\n");
+    return 0;
+}
